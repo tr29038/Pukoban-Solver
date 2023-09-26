80b1bdd1900875522e688e3e85e450f7aa51e322
tr29038 <thmire2016@gmail.com>
2023-09-25 22:18:06 -0400

    Assignment automater

---
+++ build/run_assignment_needs.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+declare -a algs=("astar" "gfs" "dfs" "bfs")
+
+for alg in "${algs[@]}"
+do
+    echo "Working with $alg..."
+
+    if [[ ! -d ./solutions/$alg ]]; then
+        echo "Created directory ./solutions/$alg"
+        mkdir -p ./solutions/$alg
+    fi
+
+    ./pukoban_solver ../puzzles/puzzle_7.txt $alg ./solutions/$alg/puzzle_7.solution
+    ./pukoban_solver ../puzzles/puzzle_6.txt $alg ./solutions/$alg/puzzle_6.solution
+    ./pukoban_solver ../puzzles/puzzle_5.txt $alg ./solutions/$alg/puzzle_5.solution
+done
